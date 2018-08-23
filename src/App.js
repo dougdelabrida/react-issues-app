d2343a8045558ab117e0f226e300462215d36d61
Douglas Delabrida <dougdelabrida@gmail.com>
2018-08-22 22:35:15 -0300

    implement basic list of issues

--- src/App.js
+++ src/App.js
@@ -3,8 +3,104 @@ import { Header, Table } from 'semantic-ui-react'
 import { Paginator } from './Paginator'
 
 class App extends Component {
+
+  state = {
+    issues: [],
+    selectedPage: 1,
+    totalPages: 0,
+    isLoading: false
+  }
+
+  async getIssues(params) {
+    let totalPages = 0
+
+    const issues = await fetch(`https://api.github.com/repos/facebook/react/issues?page=${params.page}&state=${params.state}&per_page=6`)
+      .then(response => {
+        const [, last] = response.headers.get('Link').split('rel="next"')
+        totalPages = last ? Number(last.match(/page=(.*?)&/)[1]) : params.page
+        // TODO IMPROVE
+        return response.json()
+      })
+
+      return {totalPages, issues}
+  }
+
+  handleOnSelectPage = (page) => {
+    this.setState({isLoading: true})
+
+    const params = {
+      page,
+      state: 'all'
+    }
+    this.getIssues(params).then(response => {
+      this.setState({
+        isLoading: false,
+        selectedPage: page,
+        ...response
+      })
+    })
+  }
+
+  componentDidMount() {
+    const params = {
+      page: 1,
+      state: 'all'
+    }
+
+    this.setState({isLoading: true})
+
+    this.getIssues(params).then(response => {
+      this.setState({
+        isLoading: false,
+        ...response
+      })
+    })
+  }
+
   render() {
-    return <span>React =]</span>
+    return (
+      <div className="ui container">
+        <div className={`ui transition dimmer ${this.state.isLoading ? 'active' : null}`}>
+          <div className='ui loader'/>
+        </div>
+        <header>
+          <Header as='h1'>React's Issues</Header>
+        </header>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Issue Number</Table.HeaderCell>
+              <Table.HeaderCell>Title</Table.HeaderCell>
+              <Table.HeaderCell>Created At</Table.HeaderCell>
+              <Table.HeaderCell>Updated At</Table.HeaderCell>
+              <Table.HeaderCell>Labels</Table.HeaderCell>
+              <Table.HeaderCell>State</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {this.state.issues.map(issue => (
+              <Table.Row key={issue.id}>
+                <Table.Cell>{issue.number}</Table.Cell>
+                <Table.Cell>{issue.title}</Table.Cell>
+                <Table.Cell>{issue.created_at}</Table.Cell>
+                <Table.Cell>{issue.updated_at}</Table.Cell>
+                <Table.Cell>
+                  {issue.labels.map((label, i) => (
+                    <span key={`${label.id}_${i}`} className='ui tag label' style={{backgroundColor: '#' + label.color}}>{label.name}</span>
+                  ))}
+                </Table.Cell>
+                <Table.Cell>
+                  <div className={`ui ${issue.state === 'open' ? 'green' : 'red'} label`}>{issue.state}</div>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+
+          <Paginator selectedPage={this.state.selectedPage} totalPages={this.state.totalPages} onSelectPage={this.handleOnSelectPage} />
+        </Table>
+      </div>
+    )
   }
 }
 
